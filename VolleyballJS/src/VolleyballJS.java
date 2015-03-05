b4a50692ff7b62a7f71b04ab14a130cf424ec702
volodymyr-vietrov <vetrov@gmail.com>
2015-03-05 12:25:04 +0200

    05.03.2014 Second commit

--- VolleyballJS/src/VolleyballJS.java
+++ VolleyballJS/src/VolleyballJS.java
@@ -4,6 +4,8 @@ public class VolleyballJS {
 	public static void main(String[] args) {
 		System.out.println("This is VolleyballJS.main()");
 		System.out.println("This is updated VolleyballJS.main()");
+		System.out.println("This is updated VolleyballJS.main() from local Eclipse");
+		System.out.println("This is updated VolleyballJS.main() from local Eclipse one more time");
 	}
 
 }
